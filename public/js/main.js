d5ed36694f45a0a224891ee1177737b06a6b3175
Mark Nowowiejski <marknowowiejski@CBS-MNOWOWI1-M.local>
2018-11-02 11:18:35 -0400

    button red on click v1

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,15 @@
 (function () {
+  const buttonParent = document.querySelector('div');
+  const buttonItems = buttonParent.querySelectorAll('button');
 
-  // your js goes here
+  buttonParent.addEventListener('click', setActiveColor);
 
+
+  function setActiveColor (e) {
+    for (const buttonItem of buttonItems) {
+      buttonItem.classList.remove('button-xl');
+    }
+
+    e.target.classList.add('button-danger');
+    e.stopPropagation();}
 })();
